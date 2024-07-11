12044557ff2ddc58de12b4a1e739401c9f950a26
smallproject <j.a.villas@live.com.ph>
2024-07-11 21:55:30 +0200

    opdracht 1 gemaakt temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
 import './App.css';
+import {getSoldCount} from "./helper/tempFunction.js";
+import {getStockCount} from "./helper/tempFunction.js";
+// import styles from "./helper/styles.css";
+import "./helper/styles.css";
 
 function App() {
+
+  const soldCount = getSoldCount();
+  const stockCount = getStockCount();
+
   return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
+      <>
+          <h1>Tech it easy dashboard</h1>
+          <h2>Verkoopoverzicht</h2>
+          <div className="upper_container">
+              <div id="sold">
+                  <h3>Aantal verkochte producten:</h3>
+                  <h3>{soldCount}</h3>
+              </div>
+
+              <div id="purchased">
+                  <h3>Aantal ingekochte producten Stock:</h3>
+                  <h3>{stockCount}</h3>
+              </div>
+
+              <div id="toBeSold">
+                  <h3>Aantal te verkopen producten:</h3>
+                  <h3>{stockCount - soldCount}</h3>
+              </div>
+          </div>
+
+          <div>
+              <h1>Verkoopoverzicht</h1>
+              <div className="lower_container">
+              </div>
+          </div>
+      </>
   )
 }
 
---
+++ src/helper/tempFunction.js
@@ -0,0 +1,21 @@
+import {inventory} from "../constants/inventory.js";
+
+export function getSoldCount () {
+    let count = 0;
+    for (let i = 0; i < inventory.length; i++) {
+        count += inventory[i].sold;
+    }
+
+    console.log(count);
+    return count;
+}
+
+export function getStockCount () {
+    let count = 0;
+    for (let i = 0; i < inventory.length; i++) {
+        count += inventory[i].originalStock;
+    }
+
+    console.log(count);
+    return count;
+}
